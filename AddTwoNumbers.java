92f7b594d3b3ea5784075bf1fb90aab746b3614e
NagashreeBhat <nagashreesbhat@gmail.com>
2019-11-14 15:43:18 -0800

    2 - Leetcode Problem to Add two numbers

---
+++ AddTwoNumbers.java
@@ -0,0 +1,39 @@
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode l3 = dummy;
+        int carry = 0;
+        while( l1 != null || l2 != null) {
+            int l1_val = (l1 != null) ? l1.val : 0;
+            int l2_val = (l2 != null) ? l2.val : 0;
+
+            int currentSum = l1_val + l2_val + carry;
+            carry = currentSum / 10;
+            int lastDigit = currentSum % 10;
+
+            ListNode newNode = new ListNode(lastDigit);
+            l3.next = newNode;
+            if(l1 != null) {
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                l2 = l2.next;
+            }
+            l3 = l3.next;
+        }
+        if( carry > 0) {
+            ListNode newNode = new ListNode(1);
+            l3.next = newNode;
+            l3 = l3.next;
+        }
+        return dummy.next;
+    }
+
+
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+}
